55d5e9bbf47ce5f2dd14026823c3df237b1844e0
vxrboii-spec <vxrboii7@gmail.com>
2025-08-25 13:36:08 +0400

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -3,13 +3,15 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || 'chromium-browser'
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 720 });
+
   await page.goto('https://simplebooklet.com/cpanel', { waitUntil: 'networkidle0' });
+
   await page.screenshot({ path: 'preview.png' });
+
   await browser.close();
 })();
